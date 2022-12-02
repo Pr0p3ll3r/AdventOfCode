24b0c3dd571d7dc26831d222c8614743d7aba320
Jakub Dudek <kuba395@gmail.com>
2022-12-02 13:07:16 +0100

    Initial commit

---
+++ 01/01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._01
+{
+    class _01
+    {
+        static void Main(string[] args)
+        {           
+            string[] lines = File.ReadAllLines("F:\\AdventOfCode\\01\\01.txt");
+            List<int> calories = new List<int>();
+            int cal = 0;
+            foreach (var line in lines)
+            {
+                if(line == string.Empty)
+                {
+                    calories.Add(cal);
+                    cal = 0;
+                    continue;
+                }
+                cal += int.Parse(line);
+            }
+            var sorted = calories.OrderByDescending(x => x);
+            Console.WriteLine(sorted.ElementAt(0));
+            Console.WriteLine(sorted.Take(3).Sum());
+        }
+    }
+}
